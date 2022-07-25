a3efca7ad991feb3714e689d9f7cc537c46719de
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-07-25 11:23:41 -0500

    Cambiando accesos

--- includes/database.php
+++ includes/database.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 function conexionBD()
 {
-    $db = new mysqli('localhost', 'root', '', 'uptask');
-    //$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DATABASE']);
+    $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DATABASE']);
     $db->set_charset('utf8');
 
     if (!$db) {
