6718a4db139d1a47d1016aeaeefe36220289b868
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-07-25 14:36:39 -0500

    Envio de email

--- views/dashboard/formulario-proyecto.php
+++
@@ -1,6 +0,0 @@
-<div class="campo">
-    <input type="text" id="nombre" name="nombreproyecto" placeholder="nombre" required>
-    <label for="nombre" class="label">Nombre Proyecto</label>
-</div>
-
-
